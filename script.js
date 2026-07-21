8a8196a23f6dcb39ba48cd8fcd3bf22895f0721e
Bayu <bimbaybimabayu@gmail.com>
2026-07-22 00:27:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
+const video=document.getElementById("video");
 const music=document.getElementById("music");
+const play=document.getElementById("play");
+const start=document.getElementById("start");
 
-document.body.addEventListener("click",()=>{
+play.onclick=async()=>{
 
+try{
+
+await video.play();
+
+music.currentTime=video.currentTime;
+
+await music.play();
+
+start.style.display="none";
+
+}catch(e){
+alert("Browser memblokir autoplay");
+}
+
+};
+
+video.addEventListener("timeupdate",()=>{
+
+if(Math.abs(video.currentTime-music.currentTime)>0.15){
+
+music.currentTime=video.currentTime;
+
+}
+
+});
+
+video.addEventListener("play",()=>music.play());
+
+video.addEventListener("pause",()=>music.pause());
+
+video.addEventListener("seeking",()=>{
+
+music.currentTime=video.currentTime;
+
+});
+
+video.addEventListener("ended",()=>{
+
+video.currentTime=0;
+music.currentTime=0;
+
+video.play();
 music.play();
 
-},{once:true});
+});
